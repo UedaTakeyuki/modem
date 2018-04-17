a10ebfb344b927fad3a87848082f4d24ff0b8559
Ueda Takeyuki <ueda@latelierdueda.com>
2018-04-17 10:50:39 +0900

    complete

--- getcurrentcops.py
+++ getcurrentcops.py
@@ -1,22 +1,23 @@
 import re
 import at
 
-pattern = re.compile("(([0-9][0-9][0-9])([0-9][0-9])([0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]))\r\n")
+pattern = re.compile('^\+COPS:.*"(([0-9][0-9][0-9])([0-9][0-9]))".*\\r\\n')
 
 def read():
 	imsi  = ""
 	mcc   = ""
 	mnc   = ""
 
-	lines = at.read("AT+CIMI")
+	lines = at.read("AT+COPS=3,2")
+	lines = at.read("AT+COPS?")
 
 	matchOB = re.match(pattern, lines[1])
 
 	if matchOB:
-		imsi = matchOB.group(1)
+		cop = matchOB.group(1)
 		mcc  = matchOB.group(2)
 		mnc  = matchOB.group(3)
-	return [imsi, mcc, mnc]
+	return [cop, mcc, mnc]
 
 if __name__ == '__main__':
 	print read()
