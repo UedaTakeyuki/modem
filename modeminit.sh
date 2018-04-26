a41098cf392f3f7571ae86dc674309e6063ecc7a
Ueda Takeyuki <ueda@latelierdueda.com>
2018-04-26 21:01:33 +0900

    modem initialize

--- modeminit.sh
+++ modeminit.sh
@@ -1,4 +1,10 @@
-sleep 2
-/usr/bin/python /home/pi/modem/getinfos.py > /tmp/modem.ini
-sleep 2
-/usr/bin/wvdial `python /home/pi/modem/getimsi.py -carrier` &
+#!/bin/bash -e
+
+# http://qiita.com/bsdhack/items/29400b9ed989cd67b1db
+cwd=`dirname "${0}"`
+# ${0} が 相対パスの場合は cd して pwd を取得
+expr "${0}" : "/.*" > /dev/null || cwd=`(cd "${cwd}" && pwd)`
+
+${cwd}/modeminfos.sh
+${cwd}/settimezonebymodem.sh
+${cwd}/getkcell.sh
